0d47dd10c55266470e9f07e0321f889bdb7b8ff8
OmAmbalkar <om1998.ambalkar@gmail.com>
2022-04-09 18:46:54 -0700

    Added Extra Fields

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -11,18 +11,19 @@ export const SignUp = () => {
         username: "",
         gender: "M",
         address: "",
-        district: "SantaClara",
-        state: "California",
-        taluka: "BayArea",
-        profile_pic: "link_to_picture",
-        phone_number: "66928811",
+        district: "",
+        state: "",
+        taluka: "",
+        profile_pic: "profilepic",
+        phone_number: "",
         role: "Student",
         password: ""
     }
     const [formData, setFormData] = useState(initialSignUpState);
    
     const submitSignUp = () =>{
-        if (formData.email === "" || formData.username === "" || formData.address === "" || formData.password === "") {
+        console.log(formData)
+        if (formData.email === "" || formData.username === "" || formData.address === "" || formData.password === "" || formData.state === "" || formData.district === "" || formData.taluka === "" || formData.phone_number === "" ) {
             if (formData.email === "") {
                 alert("Please Enter Email.")
             } else if (formData.username == "" ) {
@@ -31,6 +32,18 @@ export const SignUp = () => {
                 alert("Please Enter Address")
             } else if (formData.password == "" ) {
                 alert("Please Enter Password")
+            }
+            else if (formData.state == "" ) {
+                alert("Please Enter State")
+            }
+            else if (formData.district == "" ) {
+                alert("Please Enter District")
+            }
+            else if (formData.taluka == "" ) {
+                alert("Please Enter Taluka")
+            }
+            else if (formData.phone_number == "" ) {
+                alert("Please Enter Phone")
             }
         } else if (formData.password.length < 8) {
             alert("Please Enter Password with more than or equal to 8 characters")
@@ -109,8 +122,38 @@ export const SignUp = () => {
                         <input type="text" onChange={(e)=>setFormData({...formData, username: e.target.value})} value={formData.username} className="form-control" placeholder="Enter Username" />
                     </div>
                     <div className="form-group space">
+                        <label>Phone</label>
+                        <input type="number" onChange={(e)=>setFormData({...formData, phone_number: e.target.value})} value={formData.phone_number} className="form-control" placeholder="Enter Phone" />
+                    </div>
+                    <div className="form-group space">
                         <label>Address</label>
                         <input type="text" onChange={(e)=>setFormData({...formData, address: e.target.value})} value={formData.address} className="form-control" placeholder="Enter Address" />
+                    </div>
+                    <div className="form-group space">
+                        <label>District</label>
+                        <input type="text" onChange={(e)=>setFormData({...formData, district: e.target.value})} value={formData.district} className="form-control" placeholder="Enter District" />
+                    </div>
+                    <div className="form-group space">
+                        <label>State</label>
+                        <input type="text" onChange={(e)=>setFormData({...formData, state: e.target.value})} value={formData.state} className="form-control" placeholder="Enter State" />
+                    </div>
+                    <div className="form-group space">
+                        <label>Taluka</label>
+                        <input type="text" onChange={(e)=>setFormData({...formData, taluka: e.target.value})} value={formData.taluka} className="form-control" placeholder="Enter Taluka" />
+                    </div>
+                    <div className="form-group space">
+                        <label for="role">Role</label>
+                        <select className="form-control" id="role" onChange={(e)=>setFormData({...formData, role: e.target.value})} >
+                        <option value="Student">Student</option>
+                        <option value="Faculty">Faculty</option>
+                        </select>
+                    </div>
+                    <div className="form-group space">
+                        <label for="gender">Gender</label>
+                        <select className="form-control" id="gender" onChange={(e)=>setFormData({...formData, gender: e.target.value})}>
+                        <option value="M">M</option>
+                        <option value="F">F</option>
+                        </select>
                     </div>
                     <div className="form-group space">
                         <label>Password</label>
